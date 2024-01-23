365d3bedebf2a373bcb930f43476080686313f90
Prateek Santani <prateeksantani98@gmail.com>
2024-01-23 23:22:30 +0530

    DOM

--- js-practise/app.js
+++ js-practise/app.js
@@ -1,1 +1,18 @@
-console.log('10');
+// getName();
+// console.log(x);
+
+// var x = 9;
+
+// function getName(){
+//     console.log("Hello World");
+// }
+
+// console.log(getName);
+
+console.log(getname);
+
+ var getname = () =>{
+  console.log("Hello boy");
+ }
+
+
